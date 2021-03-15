9cbf3bc5c20464d2ce3707560c449aada75566e7
slumfy <valentin.robert.bransat@gmail.com>
2021-03-15 19:06:12 +0100

    ajout de lib probleme de linkage a python

--- Gomoku.py
+++ Gomoku.py
@@ -1,7 +1,10 @@
 from GoRules import GoRules
 from PyGameGo import PyGameGo
+import gomoku_lib
+
 
 if __name__ == "__main__":
+    print(gomoku_lib.sum_as_string("AABBCC", "DDDD"))
     go = GoRules()
     game_inst = PyGameGo()
     game_inst.menu(go)
