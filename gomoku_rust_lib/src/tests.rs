8451bda553543289108d4d0503406f8e2272154f
Sithi5 <ma.sithis@gmail.com>
2021-03-18 16:20:07 +0100

    pushing new pytest tests

--- gomoku_rust_lib/src/tests.rs
+++ gomoku_rust_lib/src/tests.rs
@@ -1,20 +1,24 @@
-use pyo3::prelude::*;
-use pyo3::types::PyDict;
-use pyo3::FromPyObject;
+extern crate pyo3;
 
-#[pyclass]
-struct RustDict {
-    player: i32,
-    x: i32,
-    y: i32,
-}
+use pyo3::exceptions::*;
+use pyo3::prelude::*;
+use pyo3::types::*;
 
 #[macro_export]
 #[pyfunction]
-pub fn test_getting_dict_from_python(python_dict: PyDict) {
+pub fn test_returning_dict_to_python(
+    mut map: Vec<Vec<i32>>,
+    player: i32,
+    x: i32,
+    y: i32,
+) -> PyResult<PyObject> {
     println!("In test_getting_dict_from_python.");
 
     let gil = Python::acquire_gil();
     let py = gil.python();
-    println!(python_dict.get_item("player": i32));
+    let dict = PyDict::new(py);
+
+    dict.set_item("eated_piece", 10);
+    dict.set_item("board", &map);
+    Ok(dict.to_object(py))
 }
