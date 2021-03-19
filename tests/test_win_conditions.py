f839577e2b1b30223322c82aca57f3e0aefec6fa
Sithi5 <ma.sithis@gmail.com>
2021-03-19 15:10:39 +0100

    pushing tests

--- tests/test_win_conditions.py
+++ tests/test_win_conditions.py
@@ -4,6 +4,7 @@ from go_rules import GoRules
 from py_game_go import PyGameGo
 from player import Player
 
+
 @pytest.fixture
 def white_winning_state() -> (GoRules, PyGameGo):
     go_rules = GoRules()
@@ -14,7 +15,24 @@ def white_winning_state() -> (GoRules, PyGameGo):
         game.player = go_rules.player_list[1]
         go_rules.place_stone(game.player, 11, i)
     game.player = go_rules.player_list[0]
-    go_rules.place_stone(game.player, 10, i  + 1)
+    go_rules.place_stone(game.player, 10, i + 1)
+
+    return go_rules, game
+
+
+@pytest.fixture
+def black_winning_state() -> (GoRules, PyGameGo):
+    go_rules = GoRules()
+    game = PyGameGo(sound_status=False, test_mode=True)
+    for i in range(4):
+        game.player = go_rules.player_list[0]
+        go_rules.place_stone(game.player, 10, i)
+        game.player = go_rules.player_list[1]
+        go_rules.place_stone(game.player, 11, i)
+    game.player = go_rules.player_list[0]
+    go_rules.place_stone(game.player, 9, 0)
+    game.player = go_rules.player_list[1]
+    go_rules.place_stone(game.player, 11, 4)
 
     return go_rules, game
 
@@ -23,6 +41,27 @@ def test_white_win(white_winning_state):
     go_rules, game = white_winning_state
     game.player = go_rules.player_list[1]
     state = go_rules.place_stone(game.player, 11, 4)
+    assert state == go_rules.player_list[0].nb
 
+
+def test_black_win(black_winning_state):
+    go_rules, game = black_winning_state
     game.player = go_rules.player_list[0]
-    go_rules.check_win_rust(game.player)
+    state = go_rules.place_stone(game.player, 10, 4)
+    assert state == go_rules.player_list[1].nb
+
+
+@pytest.mark.parametrize("x", [0, 1, 2, 3, 4])
+def test_black_prevent_white_win(white_winning_state, x):
+    go_rules, game = white_winning_state
+    game.player = go_rules.player_list[1]
+    state = go_rules.place_stone(game.player, 9, x)
+    assert state == 0
+
+
+@pytest.mark.parametrize("x", [0, 1, 2, 3, 4])
+def test_white_prevent_black_win(black_winning_state, x):
+    go_rules, game = black_winning_state
+    game.player = go_rules.player_list[0]
+    state = go_rules.place_stone(game.player, 12, x)
+    assert state == 0
