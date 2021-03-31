3ef7d638bfbcbc5cbc2fc987fc0b4ecc63d3e5e8
slumfy <valentin.robert.bransat@gmail.com>
2021-03-31 15:39:56 +0200

    rebase from main

--- gomoku_rust_lib/src/negamax.rs
+++ gomoku_rust_lib/src/negamax.rs
@@ -105,23 +105,6 @@ pub fn print_heuristic_table(state: &State) {
         println!("{:?}", table[x]);
     }
 }
-//example of negamax alpha beta pruning
-// function negamax(node, depth, α, β, color)
-// 	is if depth = 0 or node is a terminal node then
-// 		return color × the heuristic value of node
-
-// 	childNodes := generateMoves(node)
-// 	childNodes := orderMoves(childNodes)
-// 	value := −∞
-// 	foreach child in childNodes do
-// 		value := max(value, −negamax(child, depth − 1, −β, −α, −color))
-// 		α := max(α, value)
-// 		if α ≥ β then
-// 			break (* cut-off *)
-// 	return value Negamax
-
-// first call
-// negamax(rootNode, depth, −∞, +∞, 1)
 
 //negascout
 
