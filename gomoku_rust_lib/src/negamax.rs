12923c959c8519b5f93b8c921d16ee08294e2262
slumfy <valentin.robert.bransat@gmail.com>
2021-03-31 14:35:26 +0200

    transpo start

--- gomoku_rust_lib/src/negamax.rs
+++ gomoku_rust_lib/src/negamax.rs
@@ -156,19 +156,5 @@ pub fn print_heuristic_table(state: &State) {
     }
 }
 
-//negascout
-
-// function pvs(node, depth, α, β, color) is
-//     if depth = 0 or node is a terminal node then
-//         return color × the heuristic value of node
-//     for each child of node do
-//         if child is first child then
-//             score := −pvs(child, depth − 1, −β, −α, −color)
-//         else
-//             score := −pvs(child, depth − 1, −α − 1, −α, −color) (* search with a null window *)
-//             if α < score < β then
-//                 score := −pvs(child, depth − 1, −β, −score, −color) (* if it failed high, do a full re-search *)
-//         α := max(α, score)
-//         if α ≥ β then
-//             break (* beta cut-off *)
-//     return α
+fn transposition_table_push() {}
+fn transposition_table_search() {}
