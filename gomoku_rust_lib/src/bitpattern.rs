adb8e31184a49f521260e5d904f10cca64e023d7
slumfy <valentin.robert.bransat@gmail.com>
2021-04-07 15:25:23 +0200

    pattern order

--- gomoku_rust_lib/src/bitpattern.rs
+++ gomoku_rust_lib/src/bitpattern.rs
@@ -1,10 +1,13 @@
 use crate::bitboards::Bitboards;
 // patern need to sort by order of check
-static PATTERN: [(u8, usize, &str); 12] = [
-    (0xF8, 5, "five"),                  // five XXXXX...
-    (0x74, 6, "split four 3"),          // split four 3 .XXX.X..
-    (0x6C, 6, "split four 2"),          // split four 2 .XX.XX..
-    (0x5C, 6, "split four 1"),          // split four 1 ..X.XXX.
+static PATTERN: [(u8, usize, &str); 15] = [
+    (0xF8, 6, "five"),                  // five XXXXX...
+    (0x74, 7, "split four 3"),          // split four 3 .XXX.X..
+    (0x6C, 7, "split four 2"),          // split four 2 .XX.XX..
+    (0x5C, 7, "split four 1"),          // split four 1 .X.XXX..
+	(0xE8, 7, "close split four 3"),    // close split four 3 XXX.X...
+    (0xD8, 7, "close split four 2"),    // close split four 2 XX.XX...
+    (0xB8, 7, "close split four 1"),    // close split four 1 X.XXX...
     (0xF0, 5, "close four"),            // close four XXXX....
     (0xB0, 5, "close split three"),     // close split three X.XX....
     (0xD0, 5, "close split three rev"), // close split three rev XX.X....
@@ -13,41 +16,77 @@ static PATTERN: [(u8, usize, &str); 12] = [
     (0x68, 6, "open split three"),      // open split three .X.XX...
     (0x58, 6, "open split three rev"),  // open split three rev .XX.X...
     (0x70, 5, "open three"),			// open three .XXX....
-]; 
+];
+static BLOCKER: [(u8, usize); 5] = [
+	(0x82, 7), // X.....X.
+	(0x84, 6), // X....X..
+	(0x88, 5), // X...X...
+	(0x90, 4), // X..X....
+	(0xA0, 3), // X.X.....
+];
 
 pub fn pattern_dispatcher(bitboards: &Bitboards, pos: usize, player: i8) {
     if player == 1 {
         println!("white player pattern in row:");
-        pattern_finder(&bitboards.white_board, pos);
+        pattern_finder(&bitboards.white_board, &bitboards.black_board, pos);
     } else if player == -1 {
         println!("black player pattern in row:");
-        pattern_finder(&bitboards.black_board, pos);
+        pattern_finder(&bitboards.black_board, &bitboards.white_board, pos);
     }
 }
 
-fn pattern_finder(bitboard: &[u64; 6], pos: usize) {
-    println!("pos: {}", pos);
-    println!("x idx: {}", pos / 19);
-    println!("y idx: {}", pos % 19);
-    println!("int idx: {}", pos / 64);
+fn pattern_finder(bitboard: &[u64; 6], blocker: &[u64; 6], pos: usize) {
     let y = pos % 19;
-    let mut int_to_check = create_row(bitboard, pos);
-    println!("int checked: {:064b}", int_to_check);
-    int_to_check <<= 5;
-    for l in 0..19 {
-        let mut int_shifted = int_to_check >> l;
-        // println!("int checked shifted: {:064b}", int_shifted);
-        let casted_int = int_shifted as u8;
-
+    let mut player_row = create_row(bitboard, pos);
+	let mut blocker_row = create_row(blocker, pos);
+    println!("player checked: {:064b}", player_row);
+	println!("blocker  check: {:064b}", blocker_row);
+    player_row <<= 7;
+	blocker_row <<= 7;
+	let mut found_pattern: (&str, usize) = ("",0);
+    for l in (0..19).rev() {
+        let mut player_shifted = player_row >> l;
+		let mut blocker_shifted = blocker_row >> l;
+        let player_casted = player_shifted as u8;
+		let blocker_casted = blocker_shifted as u8;
+		let mut is_bloked: usize = 0;
         for p in 0..PATTERN.len() {
-            // println!("int checked: {:08b}", casted_int);
-            // println!("partern checked: {:08b}", PATTERN[p].0);
-            // println!("& bit operator: {:08b}", (casted_int & PATTERN[p].0));
-            if (casted_int & PATTERN[p].0) == PATTERN[p].0 {
-                println!("{} found", PATTERN[p].2);
+            if (player_casted & PATTERN[p].0) == PATTERN[p].0 {
+				// println!("player casted: {:08b}", player_casted);
+				for b in 0..BLOCKER.len() {
+					if BLOCKER[b].1 == PATTERN[p].1 {
+						// println!("blocker  cast: {:08b}", blocker_casted);
+						// println!("blocked checked: {:08b}", blocker_casted);
+           				// println!("pattern checked: {:08b}", BLOCKER[b].0);
+						let mut blocker_checker: u8 = (blocker_casted & BLOCKER[b].0);
+						// println!("BLOCKER CHECKER: {:08b}", blocker_checker);
+						// println!("l {} length {}", l , PATTERN[p].1);
+						if blocker_checker == BLOCKER[b].0 {
+							is_bloked = 2;
+						}
+						else if blocker_checker != 0 {
+							is_bloked = 1;
+							if l <= PATTERN[p].1 || l == 18 {is_bloked += 1;}
+						}
+						else if l <= PATTERN[p].1 || l == 18 {
+							is_bloked = 1;
+							if blocker_checker != 0 {is_bloked += 1;}
+						}
+						else {
+							is_bloked = 0;
+						}
+					}
+				}
+				if is_bloked < 2 {
+					found_pattern = (PATTERN[p].2.clone(), is_bloked);
+                	println!("{} found {} blocker", PATTERN[p].2, is_bloked);
+					break ;
+				}
             }
+			if found_pattern.0 != "" { break ;}
         }
     }
+	println!("PATTERN FOUND {} BLOCKER FOUND {}", found_pattern.0, found_pattern.1);
 }
 
 fn create_row(bitboard: &[u64; 6], pos: usize) -> u64 {
