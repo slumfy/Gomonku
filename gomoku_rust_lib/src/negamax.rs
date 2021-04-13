90c27625eb175f5713cd98b8bdd321872e1904a2
slumfy <valentin.robert.bransat@gmail.com>
2021-04-13 12:34:59 +0200

    fix merge

--- gomoku_rust_lib/src/negamax.rs
+++ gomoku_rust_lib/src/negamax.rs
@@ -1,4 +1,6 @@
 use crate::global_var;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
 use crate::state::create_child;
 use crate::state::state_is_terminated;
 use crate::state::State;
@@ -43,6 +45,41 @@ fn update_max_depth(depth: i32) {
 			global_var::MAX_DEPTH_REACH = global_var::DEPTH - depth;
 		}
 	}
+pub fn negamax_with_tt(mut state: &mut State, depth: i32, mut alpha: i32, beta: i32, color: i8) -> i32 {
+    let tt_search : (bool,i32,i32);
+	unsafe {tt_search = transposition_table_search(state, &TRANSPOTABLENEGA)};
+	if tt_search.0 == true && tt_search.1 >= depth {
+		return tt_search.2;
+	}
+	if depth != 0 {
+        state.available_move = create_child(&mut state);
+        state.available_move.sort_by_key(|d| Reverse(d.heuristic));
+    }
+    // println!("current state: {:?} player to play {} current heuristic {} depth {}", state.current_move, state.player_to_play, state.heuristic, depth);
+    if depth == 0 || state.available_move.len() == 0 || state_is_terminated(state) == true {
+        return state.heuristic * color as i32;
+    }
+    let mut value: i32 = -1000;
+    let len = state.available_move.len();
+    for child in 0..len {
+        let negamax = -negamax(
+            &mut state.available_move[child],
+            depth - 1,
+            -beta,
+            -alpha,
+            -color,
+        );
+        value = std::cmp::max(value, negamax);
+        alpha = std::cmp::max(alpha, value);
+        if alpha >= beta {
+            // println!("pruning");
+            break;
+        }
+    }
+	unsafe {transposition_table_push(state, depth,&mut TRANSPOTABLENEGA);}
+    // // println!("alpha {}  beta {}", alpha, beta);
+    state.heuristic = value;
+    return value;
 }
 
 pub fn negascout(mut state: &mut State, depth: i32, mut alpha: i32, beta: i32, color: i8) -> i32 {
@@ -97,9 +134,65 @@ pub fn negascout(mut state: &mut State, depth: i32, mut alpha: i32, beta: i32, c
     return alpha;
 }
 
-pub fn return_move(state: &mut State, heuristic: i32) -> ((isize, isize), i32) {
+pub fn negascout_with_tt(mut state: &mut State, depth: i32, mut alpha: i32, beta: i32, color: i8) -> i32 {
+    let tt_search : (bool,i32,i32);
+	unsafe {tt_search = transposition_table_search(state, &TRANSPOTABLESCOUT)};
+	if tt_search.0 == true && tt_search.1 >= depth {
+		return tt_search.2;
+	}
+	if depth != 0 && state.available_move.len() == 0 {
+        state.available_move = create_child(&mut state);
+        state.available_move.sort_by_key(|d| Reverse(d.heuristic));
+    }
+    // println!("current state: {:?} player to play {} current heuristic {} depth {}", state.current_move, state.player_to_play, state.heuristic, depth);
+    if depth == 0 || state.available_move.len() == 0 || state_is_terminated(state) == true {
+        state.heuristic = state.heuristic * color as i32;
+        return state.heuristic * color as i32;
+    }
+    let mut value: i32;
     let len = state.available_move.len();
-    print_heuristic_table(state);
+    for child in 0..len {
+		if child == 0 {
+			value = -negascout(
+				&mut state.available_move[child],
+				depth - 1,
+				-beta,
+				-alpha,
+				-color,
+			);
+		}
+		else {
+			value = -negascout(
+				&mut state.available_move[child],
+				depth - 1,
+				-alpha -1,
+				-alpha,
+				-color,
+			);	
+		
+		if alpha < value && value < beta {
+        value = -negascout(
+            &mut state.available_move[child],
+            depth - 1,
+            -beta,
+            -value,
+            -color,
+        );
+	}
+	}
+        alpha = std::cmp::max(alpha, value);
+        if alpha >= beta {
+            // println!("pruning");
+            break;
+        }
+    }
+	unsafe {transposition_table_push(state, depth,&mut TRANSPOTABLESCOUT);}
+    // // println!("alpha {}  beta {}", alpha, beta);
+    state.heuristic = alpha;
+    return alpha;
+}
+
+print_heuristic_table(state);
 	unsafe{println!("MAX DEPTH: {}", global_var::MAX_DEPTH_REACH);}
     // println!("heuristic of returned move : {:?}", heuristic);
     state.available_move.sort_by_key(|d| Reverse(d.heuristic));
@@ -163,5 +256,39 @@ pub fn print_heuristic_table(state: &State) {
     }
 }
 
+<<<<<<< HEAD
 fn transposition_table_push() {}
 fn transposition_table_search() {}
+=======
+struct Transpotablenode {
+	hash : u64,
+	depth: i32,
+	value: i32
+}
+
+
+fn transposition_table_push(state: &State, depth: i32, transpo_table:&mut Vec<Transpotablenode>) {
+	let mut hash = DefaultHasher::new();
+	state.board.hash(&mut hash);
+	let state_hash: u64 = hash.finish();
+	let new_table_node = Transpotablenode {
+		hash: state_hash,
+		depth : depth,
+		value : state.heuristic
+	};
+	transpo_table.push(new_table_node);
+}
+
+unsafe fn transposition_table_search(state: &State, transpo_table: &Vec<Transpotablenode>) -> (bool,i32,i32) {
+	let mut hash = DefaultHasher::new();
+	state.board.hash(&mut hash);
+	let state_hash: u64 = hash.finish();
+	let len = transpo_table.len();
+	for node in 0..len {
+		if transpo_table[node].hash == state_hash {
+			return (true, transpo_table[node].depth,transpo_table[node].value);
+		}
+	}
+	return(false, 0, 0);
+}
+>>>>>>> 565b4de (status)
