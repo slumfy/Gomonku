36847011a3bb5073d63fbb58166f6c673d584f19
Sithi5 <ma.sithis@gmail.com>
2021-04-16 17:22:37 +0200

    improve test pattern_axe_finder.

--- gomoku_rust_lib/src/test_pattern_axes_finder.rs
+++ gomoku_rust_lib/src/test_pattern_axes_finder.rs
@@ -4,13 +4,17 @@ use crate::bitboards::create_bits_axes_from_pos;
 use crate::bitboards::Bitboards;
 use crate::bitpattern::pattern_axes_finder;
 use crate::global_var;
+use crate::print::print_axes;
+use crate::print::print_board_from_bitboard;
 
 pub fn test_pattern_axes_finder() {
     let mut bitboards: Bitboards = Bitboards {
         white_board: [0, 0, 0, 0, 0, 0],
         black_board: [0, 0, 0, 0, 0, 0],
     };
-    let mut pos = 0;
+
+    // Alignement of 1
+    let mut pos = 19;
     let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
     let returned_pattern = pattern_axes_finder(
         &mut bitboards,
@@ -19,5 +23,162 @@ pub fn test_pattern_axes_finder() {
         pos,
         global_var::PLAYER_WHITE_NB,
     )[0];
-    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 3)])
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 3)]);
+
+    // Alignement of 2
+    apply_bit(&mut bitboards, pos + 1, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 3)]);
+
+    // Alignement of 3
+    apply_bit(&mut bitboards, pos + 2, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (5, 1)]);
+
+    // Alignement of 4
+    apply_bit(&mut bitboards, pos + 3, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (1, 1)]);
+    test_alignment_of_five();
+}
+
+fn test_alignment_of_five() {
+    let mut bitboards: Bitboards = Bitboards {
+        white_board: [0, 0, 0, 0, 0, 0],
+        black_board: [0, 0, 0, 0, 0, 0],
+    };
+
+    let mut pos = 19;
+
+    // Alignement of 5 undefeatable
+    apply_bit(&mut bitboards, pos, global_var::PLAYER_WHITE_NB);
+    apply_bit(&mut bitboards, pos + 1, global_var::PLAYER_WHITE_NB);
+    apply_bit(&mut bitboards, pos + 2, global_var::PLAYER_WHITE_NB);
+    apply_bit(&mut bitboards, pos + 3, global_var::PLAYER_WHITE_NB);
+    apply_bit(&mut bitboards, pos + 4, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 5), (0, 5), (0, 5), (0, 5)]);
+
+    // Alignement of 5 defeatable
+    apply_bit(&mut bitboards, pos + 19, global_var::PLAYER_WHITE_NB);
+    apply_bit(&mut bitboards, pos - 19, global_var::PLAYER_BLACK_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 undefeatable
+    apply_bit(&mut bitboards, pos + 38, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 5), (0, 5), (0, 5), (0, 5)]);
+
+    // Alignement of 5 defeatable
+    apply_bit(&mut bitboards, pos + 21, global_var::PLAYER_WHITE_NB);
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 defeatable testing if pattern match in any pos
+    let axes = create_bits_axes_from_pos(pos + 1, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos + 1,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 defeatable testing if pattern match in any pos
+
+    let axes = create_bits_axes_from_pos(pos + 2, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos + 2,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 defeatable testing if pattern match in any pos
+    let axes = create_bits_axes_from_pos(pos + 3, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos + 3,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 defeatable testing if pattern match in any pos
+    let axes = create_bits_axes_from_pos(pos + 4, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos + 4,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 3), (0, 3), (0, 3), (0, 0)]);
+
+    // Alignement of 5 undefeatable
+    apply_bit(&mut bitboards, pos + 41, global_var::PLAYER_WHITE_NB);
+
+    let axes = create_bits_axes_from_pos(pos, &mut bitboards, global_var::PLAYER_WHITE_NB);
+    let returned_pattern = pattern_axes_finder(
+        &mut bitboards,
+        &axes[0],
+        &axes[1],
+        pos,
+        global_var::PLAYER_WHITE_NB,
+    )[0];
+    assert_eq!(returned_pattern, [(0, 5), (0, 5), (0, 5), (0, 5)]);
 }
