98dac641e2c4812a5bf90640367a264e0ae4b5d9
Sithi5 <ma.sithis@gmail.com>
2021-04-16 11:39:46 +0200

    renaming var

--- gomoku_rust_lib/src/algorithms.rs
+++ gomoku_rust_lib/src/algorithms.rs
@@ -24,9 +24,9 @@ pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i32, beta: i32, col
 
     let mut value: i32 = global_var::HEURISTIC_MIN_VALUE;
     let len = state.available_move.len();
-    for child in 0..len {
+    for child_index in 0..len {
         let negamax_value = -negamax(
-            &mut state.available_move[child],
+            &mut state.available_move[child_index],
             depth - 1,
             -beta,
             -alpha,
