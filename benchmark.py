1d28ad2a0a40d0e9574923febf3a92b0f27c5b0f
Sithi5 <ma.sithis@gmail.com>
2021-05-14 12:27:24 +0200

    update benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,13 @@
 import subprocess
 
-# Trying to import the gomoku_rust lib, if not compiled, execute the script to compile it.
-try:
-    import gomoku_rust
-except ImportError:
-    # Build rust lib
-    process = subprocess.Popen("rust_compilation_release.sh", shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    import gomoku_rust
+# # Trying to import the gomoku_rust lib, if not compiled, execute the script to compile it.
+# try:
+#     import gomoku_rust
+# except ImportError:
+#     # Build rust lib
+process = subprocess.Popen("rust_compilation_release.sh", shell=True, stdout=subprocess.PIPE)
+process.wait()
+import gomoku_rust
 
 
 def benchmark_test():
