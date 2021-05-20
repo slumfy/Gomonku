684a92623cc39745adbb87466e0bbb04549d7339
Sithi5 <ma.sithis@gmail.com>
2021-05-20 12:34:24 +0200

    use global.

--- gomoku_rust_lib/src/state.rs
+++ gomoku_rust_lib/src/state.rs
@@ -2,11 +2,11 @@
 
 use crate::data_struct::Bitboards;
 use crate::data_struct::State;
+use crate::global_var;
 use crate::heuristic::heuristic;
 use crate::heuristic_ratios;
 use crate::BoardStateInfo;
 use std::cmp::Reverse;
-
 
 use crate::search_space::get_search_box_bitboard;
 
@@ -61,16 +61,16 @@ pub fn create_child(state: &mut State) -> Vec<State> {
     let index_box: Vec<usize> = get_search_box_bitboard(&state.bitboards);
     let len = index_box.len();
     childs_list = Vec::new();
-	let mut saved_child = create_new_state(
-		&mut state.bitboards.clone(),
-		-state.current_player,
-		index_box[0],
-		state.white_captured_stone,
-		state.black_captured_stone,
-		state.win_state,
-		0,
-	);
-	saved_child.heuristic = heuristic_ratios::HEURISTIC_MIN_VALUE - 1;
+    let mut saved_child = create_new_state(
+        &mut state.bitboards.clone(),
+        -state.current_player,
+        index_box[0],
+        state.white_captured_stone,
+        state.black_captured_stone,
+        state.win_state,
+        0,
+    );
+    saved_child.heuristic = heuristic_ratios::HEURISTIC_MIN_VALUE - 1;
     for pos in 0..len {
         copy_bitboards = state.bitboards.clone();
         let bit_current_move_pos: usize = index_box[pos];
@@ -89,50 +89,52 @@ pub fn create_child(state: &mut State) -> Vec<State> {
             nb_move_to_win,
         );
         child.heuristic = heuristic(&mut child);
-		if child.is_playable == 0 && (saved_child.bit_current_move_pos == 0 || child.heuristic > saved_child.heuristic) {
-			saved_child = child.clone();
-		}
-		//TEST improved selection
-		let mut playable: bool = false;
-		if child.heuristic == heuristic_ratios::HEURISTIC_MAX_VALUE {
-			childs_list.clear();
-			childs_list.push(child);
-			break;
-		}
-        if child.is_playable == 0 {
-			for x in 0..4 {
-				if child.board_info.pattern_axe[x].1 != 3 {
-					playable = true;
-				}
-				if child.board_info.blocker_axe[x].1 != 3 {
-					playable = true;
-				}
-			}
+        if child.is_playable == global_var::VALID_MOVE
+            && (saved_child.bit_current_move_pos == 0 || child.heuristic > saved_child.heuristic)
+        {
+            saved_child = child.clone();
         }
-		if playable == true {
-			childs_list.push(child);
-		}
+        //TEST improved selection
+        let mut playable: bool = false;
+        if child.heuristic == heuristic_ratios::HEURISTIC_MAX_VALUE {
+            childs_list.clear();
+            childs_list.push(child);
+            break;
+        }
+        if child.is_playable == 0 {
+            for x in 0..4 {
+                if child.board_info.pattern_axe[x].1 != 3 {
+                    playable = true;
+                }
+                if child.board_info.blocker_axe[x].1 != 3 {
+                    playable = true;
+                }
+            }
+        }
+        if playable == true {
+            childs_list.push(child);
+        }
     }
-	if childs_list.len() == 0 {
-		childs_list.push(saved_child);
-	}
-	// END TEST
-	// for child in 0..childs_list.len(){
-	// 	println!("childlist {:?}",childs_list[child].bit_current_move_pos);
-	// }
-	childs_list.sort_by_key(|d| Reverse(d.heuristic));
-	let mut new_list: Vec<State> = Vec::new();
-	let mut len = childs_list.len();
-	if len > 5 {
-		len = 5;
-	}
-	for child in 0..len {
-		new_list.push(childs_list[child].clone());
-	}
-	return new_list;
-	// if childs_list.len() > 1 {
-	// childs_list = reduce_child_list(childs_list);
-	// }
+    if childs_list.len() == 0 {
+        childs_list.push(saved_child);
+    }
+    // END TEST
+    // for child in 0..childs_list.len(){
+    // 	println!("childlist {:?}",childs_list[child].bit_current_move_pos);
+    // }
+    childs_list.sort_by_key(|d| Reverse(d.heuristic));
+    let mut new_list: Vec<State> = Vec::new();
+    let mut len = childs_list.len();
+    if len > 5 {
+        len = 5;
+    }
+    for child in 0..len {
+        new_list.push(childs_list[child].clone());
+    }
+    return new_list;
+    // if childs_list.len() > 1 {
+    // childs_list = reduce_child_list(childs_list);
+    // }
     // return childs_list;
 }
 
@@ -187,7 +189,6 @@ pub fn create_child(state: &mut State) -> Vec<State> {
 // 	// return reduce_list;
 // 	return max_reduce_list;
 // }
-
 
 pub fn state_is_terminated(state: &mut State) -> bool {
     if state.white_captured_stone >= 10 || state.black_captured_stone >= 10 {
