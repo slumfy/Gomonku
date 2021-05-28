083a060c3c14d3cfc7d8ed099b76591b05a078cf
Sithi5 <ma.sithis@gmail.com>
2021-05-28 12:41:09 +0200

    update negamax cumulative heuristic nerf.

--- gomoku_rust_lib/src/algorithms/negamax.rs
+++ gomoku_rust_lib/src/algorithms/negamax.rs
@@ -46,16 +46,16 @@ pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i64, beta: i64) -> 
             break;
         }
     }
-    if (state.heuristic as i128 - (value / 2) as i128)
+    if (state.heuristic as i128 - (value / 5) as i128)
         > heuristic_ratios::HEURISTIC_MAX_VALUE as i128
     {
         state.heuristic = heuristic_ratios::HEURISTIC_MAX_VALUE;
-    } else if (state.heuristic as i128 - (value / 2) as i128)
+    } else if (state.heuristic as i128 - (value / 5) as i128)
         < heuristic_ratios::HEURISTIC_MIN_VALUE as i128
     {
         state.heuristic = heuristic_ratios::HEURISTIC_MIN_VALUE;
     } else {
-        state.heuristic = state.heuristic - value / 2;
+        state.heuristic = state.heuristic - value / 5;
     }
     return state.heuristic;
 }
