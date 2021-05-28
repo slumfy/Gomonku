604d97586651c12f9fb2c545ca08289131d7418c
slumfy <valentin.robert.bransat@gmail.com>
2021-05-28 14:35:08 +0200

    fix out of bound

--- gomoku_rust_lib/src/algorithms/negamax.rs
+++ gomoku_rust_lib/src/algorithms/negamax.rs
@@ -9,29 +9,32 @@ use crate::heuristic_ratios;
 use crate::state::create_child;
 use crate::state::state_is_terminated;
 use std::cmp::Reverse;
+use crate::global_var;
 
 pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i64, mut beta: i64) -> i64 {
 	let alphaorigin = alpha;
     update_node_checked_count();
     update_max_depth(depth);
-	let tt_entry = transpotable::tt_search(state);
-	if tt_entry != None {
-		let tt_unwrap = tt_entry.unwrap();
-		if tt_unwrap.depth >= depth {
-			// println!("TTentry {:?}",tt_unwrap);
-			if tt_unwrap.flag == Flag::EXACT {
-				update_tt_count();
-				return tt_unwrap.value;
-			}
-			else if tt_unwrap.flag == Flag::LOWERBOUND {
-				alpha = std::cmp::max(alpha, tt_unwrap.value);
-			}
-			else if tt_unwrap.flag == Flag::UPPERBOUND {
-				beta = std::cmp::min(beta, tt_unwrap.value);
-			}
-			if alpha >= beta {
-				update_tt_count();
-				return tt_unwrap.value;
+	if unsafe { depth != global_var::DEPTH } {
+		let tt_entry = transpotable::tt_search(state);
+		if tt_entry != None {
+			let tt_unwrap = tt_entry.unwrap();
+			if tt_unwrap.depth >= depth {
+				// println!("TTentry {:?}",tt_unwrap);
+				if tt_unwrap.flag == Flag::EXACT {
+					update_tt_count();
+					return tt_unwrap.value;
+				}
+				else if tt_unwrap.flag == Flag::LOWERBOUND {
+					alpha = std::cmp::max(alpha, tt_unwrap.value);
+				}
+				else if tt_unwrap.flag == Flag::UPPERBOUND {
+					beta = std::cmp::min(beta, tt_unwrap.value);
+				}
+				if alpha >= beta {
+					update_tt_count();
+					return tt_unwrap.value;
+				}
 			}
 		}
 	}
@@ -76,14 +79,16 @@ pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i64, mut beta: i64)
         state.heuristic = state.heuristic - value / 5;
     }
 	//TT STORING
-	if state.heuristic <= alphaorigin {
-		transpotable::tt_insert(state,depth,Flag::UPPERBOUND);
-	}
-	else if state.heuristic >= beta {
-        transpotable::tt_insert(state,depth,Flag::LOWERBOUND);
-	}
-	else {
-        transpotable::tt_insert(state,depth,Flag::EXACT);
+	if unsafe { depth != global_var::DEPTH } {
+		if state.heuristic <= alphaorigin {
+			transpotable::tt_insert(state,depth,Flag::UPPERBOUND);
+		}
+		else if state.heuristic >= beta {
+			transpotable::tt_insert(state,depth,Flag::LOWERBOUND);
+		}
+		else {
+			transpotable::tt_insert(state,depth,Flag::EXACT);
+		}
 	}
     return state.heuristic;
 }
