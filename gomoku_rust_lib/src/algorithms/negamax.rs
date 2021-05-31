39b5d330fd2428b95dd21d45aa03692f105b3b22
Sithi5 <ma.sithis@gmail.com>
2021-05-31 14:48:00 +0200

    He his good

--- gomoku_rust_lib/src/algorithms/negamax.rs
+++ gomoku_rust_lib/src/algorithms/negamax.rs
@@ -3,41 +3,39 @@ use crate::algorithms::algo_utils::update_node_checked_count;
 use crate::algorithms::algo_utils::update_pruning_count;
 use crate::algorithms::algo_utils::update_tt_count;
 use crate::algorithms::transpotable;
-use crate::data_struct::State;
 use crate::data_struct::Flag;
+use crate::data_struct::State;
+use crate::global_var;
 use crate::heuristic_ratios;
 use crate::state::create_child;
 use crate::state::state_is_terminated;
 use std::cmp::Reverse;
-use crate::global_var;
 
 pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i64, mut beta: i64) -> i64 {
-	let alphaorigin = alpha;
+    let alphaorigin = alpha;
     update_node_checked_count();
     update_max_depth(depth);
-	if unsafe { depth != global_var::DEPTH } {
-		let tt_entry = transpotable::tt_search(state);
-		if tt_entry != None {
-			let tt_unwrap = tt_entry.unwrap();
-			if tt_unwrap.depth >= depth {
-				// println!("TTentry {:?}",tt_unwrap);
-				if tt_unwrap.flag == Flag::EXACT {
-					update_tt_count();
-					return tt_unwrap.value;
-				}
-				else if tt_unwrap.flag == Flag::LOWERBOUND {
-					alpha = std::cmp::max(alpha, tt_unwrap.value);
-				}
-				else if tt_unwrap.flag == Flag::UPPERBOUND {
-					beta = std::cmp::min(beta, tt_unwrap.value);
-				}
-				if alpha >= beta {
-					update_tt_count();
-					return tt_unwrap.value;
-				}
-			}
-		}
-	}
+    if unsafe { depth != global_var::DEPTH } {
+        let tt_entry = transpotable::tt_search(state);
+        if tt_entry != None {
+            let tt_unwrap = tt_entry.unwrap();
+            if tt_unwrap.depth >= depth {
+                // println!("TTentry {:?}",tt_unwrap);
+                if tt_unwrap.flag == Flag::EXACT {
+                    update_tt_count();
+                    return tt_unwrap.value;
+                } else if tt_unwrap.flag == Flag::LOWERBOUND {
+                    alpha = std::cmp::max(alpha, tt_unwrap.value);
+                } else if tt_unwrap.flag == Flag::UPPERBOUND {
+                    beta = std::cmp::min(beta, tt_unwrap.value);
+                }
+                if alpha >= beta {
+                    update_tt_count();
+                    return tt_unwrap.value;
+                }
+            }
+        }
+    }
     if depth == 0 || state_is_terminated(state) == true {
         return state.heuristic;
     }
@@ -78,17 +76,15 @@ pub fn negamax(mut state: &mut State, depth: i32, mut alpha: i64, mut beta: i64)
     } else {
         state.heuristic = state.heuristic - value / 5;
     }
-	//TT STORING
-	if unsafe { depth != global_var::DEPTH } {
-		if state.heuristic <= alphaorigin {
-			transpotable::tt_insert(state,depth,Flag::UPPERBOUND);
-		}
-		else if state.heuristic >= beta {
-			transpotable::tt_insert(state,depth,Flag::LOWERBOUND);
-		}
-		else {
-			transpotable::tt_insert(state,depth,Flag::EXACT);
-		}
-	}
+    //TT STORING
+    if unsafe { depth != global_var::DEPTH } {
+        if state.heuristic <= alphaorigin {
+            transpotable::tt_insert(state, depth, Flag::UPPERBOUND);
+        } else if state.heuristic >= beta {
+            transpotable::tt_insert(state, depth, Flag::LOWERBOUND);
+        } else {
+            transpotable::tt_insert(state, depth, Flag::EXACT);
+        }
+    }
     return state.heuristic;
 }
--- gomoku_rust_lib/src/heuristic_ratios.rs
+++ gomoku_rust_lib/src/heuristic_ratios.rs
@@ -4,14 +4,14 @@ pub static HEURISTIC_MIN_VALUE: i64 = i64::MIN + 1;
 pub static HEURISTIC_PREVENT_OPPONENT_WIN_BY_CAPTURE: i64 = HEURISTIC_UNBLOCKABLE_FIVE_IN_A_ROW * 5;
 pub static HEURISTIC_UNBLOCKABLE_FIVE_IN_A_ROW: i64 = HEURISTIC_FIVE_IN_A_ROW * 5;
 pub static HEURISTIC_FIVE_IN_A_ROW: i64 = HEURISTIC_DOUBLE_BLOCK_FOUR_IN_A_ROW * 5;
-pub static HEURISTIC_DOUBLE_BLOCK_FOUR_IN_A_ROW: i64 = HEURISTIC_FREE_FOUR_IN_A_ROW * 5;
+pub static HEURISTIC_DOUBLE_BLOCK_FOUR_IN_A_ROW: i64 = HEURISTIC_FREE_FOUR_IN_A_ROW;
 pub static HEURISTIC_FREE_FOUR_IN_A_ROW: i64 = HEURISTIC_DOUBLE_BLOCK_SPLIT_THREE_IN_A_ROW * 5;
 pub static HEURISTIC_DOUBLE_BLOCK_SPLIT_THREE_IN_A_ROW: i64 =
     HEURISTIC_SIMPLE_BLOCK_THREE_IN_A_ROW * 5;
-pub static HEURISTIC_SIMPLE_BLOCK_THREE_IN_A_ROW: i64 = HEURISTIC_PREVENT_CAPTURE_STONE * 5;
-pub static HEURISTIC_PREVENT_CAPTURE_STONE: i64 = HEURISTIC_FREE_THREE_IN_A_ROW * 5;
+pub static HEURISTIC_SIMPLE_BLOCK_THREE_IN_A_ROW: i64 = HEURISTIC_CAPTURE_STONE;
+pub static HEURISTIC_CAPTURE_STONE: i64 = HEURISTIC_FREE_THREE_IN_A_ROW * 5;
 pub static HEURISTIC_FREE_THREE_IN_A_ROW: i64 = HEURISTIC_SIMPLE_BLOCK_TWO_IN_A_ROW * 5;
-pub static HEURISTIC_SIMPLE_BLOCK_TWO_IN_A_ROW: i64 = HEURISTIC_FREE_TWO_IN_A_ROW * 5;
+pub static HEURISTIC_SIMPLE_BLOCK_TWO_IN_A_ROW: i64 = HEURISTIC_FREE_TWO_IN_A_ROW;
 pub static HEURISTIC_FREE_TWO_IN_A_ROW: i64 = HEURISTIC_FOUR_IN_A_ROW_WITH_ONE_BLOCKER * 5;
 pub static HEURISTIC_FOUR_IN_A_ROW_WITH_ONE_BLOCKER: i64 = 10;
 pub static HEURISTIC_THREE_IN_A_ROW_ONE_BLOCKER: i64 = HEURISTIC_POSSIBLE_AXE_DEVELOPMENT * 5;
@@ -38,16 +38,12 @@ pub static HEURISTIC_PATTERN: [[i64; 3]; 9] = [
     [HEURISTIC_FREE_THREE_IN_A_ROW / 3, 0, 0], // split four 2 .XX.XX..
     [
         HEURISTIC_FREE_THREE_IN_A_ROW,
-        HEURISTIC_PREVENT_CAPTURE_STONE,
-        HEURISTIC_PREVENT_CAPTURE_STONE / 3,
+        HEURISTIC_CAPTURE_STONE,
+        HEURISTIC_CAPTURE_STONE / 3,
     ], // three  .XXX....
     [HEURISTIC_FREE_THREE_IN_A_ROW / 2, 0, 0], // split three .X.XX...
     [HEURISTIC_FREE_THREE_IN_A_ROW / 2, 0, 0], // split three rev .XX.X...
-    [
-        HEURISTIC_FREE_TWO_IN_A_ROW,
-        -HEURISTIC_PREVENT_CAPTURE_STONE,
-        0,
-    ], // double 	.XX.....
+    [HEURISTIC_FREE_TWO_IN_A_ROW, -HEURISTIC_CAPTURE_STONE, 0], // double 	.XX.....
 ];
 
 pub static HEURISTIC_BLOCKER: [[i64; 3]; 9] = [
@@ -64,7 +60,7 @@ pub static HEURISTIC_BLOCKER: [[i64; 3]; 9] = [
 
 pub fn exponential_heuristic_capture_stone_calculator(opponent_stone_captured: i8) -> i64 {
     // let mut multiplier = opponent_stone_captured / 2;
-    // let mut value = HEURISTIC_PREVENT_CAPTURE_STONE;
+    // let mut value = HEURISTIC_CAPTURE_STONE;
     if opponent_stone_captured >= 8 {
         return HEURISTIC_PREVENT_OPPONENT_WIN_BY_CAPTURE;
     }
@@ -72,5 +68,5 @@ pub fn exponential_heuristic_capture_stone_calculator(opponent_stone_captured: i
     //     value = value * 2;
     //     multiplier -= 1;
     // }
-    return HEURISTIC_PREVENT_CAPTURE_STONE;
+    return HEURISTIC_CAPTURE_STONE;
 }
